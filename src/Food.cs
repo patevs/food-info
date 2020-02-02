976d6239648b9cdeed988d8495a62024dacc289f
patevs <ppevans11@gmail.com>
2020-02-03 11:47:36 +1300

    ✨  Food.cs

--- src/Food.cs
+++ src/Food.cs
@@ -1,5 +1,5 @@
 /**
- *  2_food/Food.cs
+ *  src/Food.cs
  */
 
 using System;
@@ -10,26 +10,21 @@ namespace Food
 {
     class Program
     {
-      /*
-      public static string Get(string uri)
+
+      /***************
+       * * CONSTANTS *
+       ***************/
+
+      private const string APP_ID = "cdfcb58a";
+      private const string APP_KEY = "1bf534f78ce5ecb6542063ce18f2db89";
+      private const string API_ENDPOINT = "https://api.edamam.com/api/food-database/parser";
+
+      /***************
+       * * FUNCTIONS *
+       ***************/
+
+      public static void GetRequest(string uri)
       {
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-        using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-        using(Stream stream = response.GetResponseStream())
-        using(StreamReader reader = new StreamReader(stream))
-        {
-          string result = reader.ReadToEnd();
-          Console.WriteLine(result);
-          return result;
-        }
-      }
-      */
-
-      public static void GetRequest(string uri) {
-        // 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id={your app_id}&app_key={your app_key}'
-        // string uri = @"https://api.edamam.com/api/food-database/parser";
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
         // request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
@@ -43,15 +38,21 @@ namespace Food
         }
       }
 
+      public static string BuildRequest(){
+        string uri = @API_ENDPOINT + "?ingr=apple&app_id=" + APP_ID + "&app_key=" + APP_KEY;
+        return uri;
+      }
+
+      /*****************************
+       * * APPLICATION ENTRY POINT *
+       *****************************/
+
       static void Main(string[] args)
       {
         Console.WriteLine("\n --- FOOD DATABASE --- \n");
         // construct url
-        // string app_id = "cdfcb58a";
-        // string app_key = "1bf534f78ce5ecb6542063ce18f2db89";
-        // string endpoint = "https://api.edamam.com/api/food-database/parser";
-        // string uri = @endpoint + "?ingr=red%20apple&app_id=" + app_id + "&app_key" + app_key;
-        string uri = @"https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=cdfcb58a&app_key=1bf534f78ce5ecb6542063ce18f2db89";
+        string uri = BuildRequest();
+        // Make get request
         GetRequest(uri);
       }
     }
