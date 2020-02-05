f1cdf9c493fa0c7001579247b40df4ef5122c452
patevs <ppevans11@gmail.com>
2020-02-05 15:10:42 +1300

    ✨  Formatting Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -87,14 +87,12 @@ namespace food_app
 
       private static void Run()
       {
-        // Console.ForegroundColor = ConsoleColor.White;
         Console.Write(" Enter a food item to lookup : ");
         Console.ForegroundColor = ConsoleColor.Green;
         // Accept input from the user
         string query = Console.ReadLine();
         Console.ResetColor();
         Console.WriteLine();
-        // Console.ForegroundColor = ConsoleColor.White;
         // Check query length
         if(query.Length < 2) {
           Colors.WriteLine(" Query must be greater than 2 letters... ".Red());
