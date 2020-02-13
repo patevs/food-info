59cacaf61a5916352593f5b0fc9dc52f8bac749a
patevs <ppevans11@gmail.com>
2020-02-13 14:13:29 +1300

    ✨  Created src/Logger.cs

---
+++ src/Logger.cs
@@ -0,0 +1,29 @@
+/**
+ *  src/Logger.cs
+ */
+
+using System;
+// using System.IO;
+
+using Alba.CsConsoleFormat.Fluent;
+
+namespace food_info
+{
+  class Logger
+  {
+      public void PrintWelcome()
+      {
+        // Clear the console
+        Console.Clear();
+        // Print welcome message
+        // Console.WriteLine("\n --- FOOD DATABASE --- \n");
+        Colors.WriteLine(
+          "\n ---- ",
+          " WELCOME TO THE FOOD DATABASE ".Black().OnGreen(),
+          " ---- \n"
+        );
+      }
+  }
+}
+
+/* EOF */
